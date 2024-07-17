1e444496a6135b75b8c703fc7e39b68fdcc32250
WeissbergAA <127210488+WeissbergAA@users.noreply.github.com>
2024-07-17 12:13:26 +0300

    Update and rename Newcalculator.java to CalculatorFinal.java

--- src/main/java/calculator/CalculatorFinal.java
+++ src/main/java/calculator/CalculatorFinal.java
@@ -2,7 +2,7 @@ package org.example;
 
 import java.util.Scanner;
 
-public class Newcalculator {
+public class CalculatorFinal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
