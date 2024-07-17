70f6ee04a06646a1ee8345c0cf4b5f76ea731439
WeissbergAA <127210488+WeissbergAA@users.noreply.github.com>
2024-07-17 12:40:19 +0300

    Update CalculatorNewTest.java

--- src/test/java/calculator/CalculatorNewTest.java
+++ src/test/java/calculator/CalculatorNewTest.java
@@ -1,19 +1,26 @@
-import Calculator.CalculatorFinal;
+package calculator;
+
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import сalculator.CalculatorFinal;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertThrows;
 
+
+
+@Feature("Вычсиления калькулятора")
 public class CalculatorNewTest {
 
-    @Test(dataProvider = "calculationData")
+    @Test (dataProvider = "calculationData")
+    @Story("Проверка базовых вычислений")
     public void testCalculation(double num1, char operator, double num2, double expected) {
         double actual = CalculatorFinal.calculate(num1, operator, num2);
         assertEquals(actual, expected);
     }
-
     @DataProvider(name = "calculationData")
-    public Object[][] calculationData() {
+    public Object[][] testcalculationData() {
         return new Object[][] {
                 {10.0, '+', 3.0, 13.0},
                 {5.0, '-', 3.0, 2.0},
@@ -23,6 +30,7 @@ public class CalculatorNewTest {
     }
 
     @Test
+    @Story("Проверка число делить на ноль")
     public void testInvalidTwoOperatorZero() {
         double num1 = 6.0;
         char operator = '/';
@@ -32,6 +40,7 @@ public class CalculatorNewTest {
     }
 
     @Test
+    @Story("Проверка ноль делить на число")
     public void testInvalidFirstOperatorZero() {
         double num1 = 0.0;
         char operator = '%';
