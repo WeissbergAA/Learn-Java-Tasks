d385f100b882a5df147d6da5d194c1e90fb54bc1
WeissbergAA <127210488+WeissbergAA@users.noreply.github.com>
2024-07-17 12:09:42 +0300

    Update and rename Calculator_v3.java to Calculator3.java

--- src/main/java/calculator/Calculator3.java
+++ src/main/java/calculator/Calculator3.java
@@ -2,7 +2,7 @@ package org.example;
 
 import java.util.Scanner;
 
-public class Calculator_v3 { // Объявление класса Calculator
+public class Calculator3 { // Объявление класса Calculator
     public static void main(String[] args) { // Точка входа в программу
         Scanner scanner = new Scanner(System.in); // Создание объекта Scanner для ввода данных с клавиатуры
 
